ea90a380146caa68cc74495d3b3a24f51ddfdeae
JeremyAllen0 <31930157+JeremyAllen0@users.noreply.github.com>
2017-10-12 10:08:26 -0500

    Edited by Jeremy Allen
    Edited by Jeremy Allen
    changed it to where it loops 30 times before it quits

--- Three_Digit_Acending_Descending_Selection/Three_Digit_Acending_Descending_Selection/Source.cpp
+++ Three_Digit_Acending_Descending_Selection/Three_Digit_Acending_Descending_Selection/Source.cpp
@@ -2,6 +2,7 @@
 Luke Hage, Logan Smith
 Three_Digit_Ascending_Descending_Selection
 Display ascending, descending or neither based on user's 3-digit number
+Edited by Jeremy Allen
 */
 
 //Libraries
@@ -22,21 +23,24 @@ void pause() {
 
 //MAIN
 void main() {
+	int i = 0;
+	for (;i <= 30; i++) {
 	int x;
-	cout << "Input 3 digit positive integer. . ." << endl;
+	cout << "Input 3 digit positive integer. . ." << endl; //Asks the user to input a 3 digit number
 	cin >> x;
-	int a = (x / 100);
-	int b = (x % 100) / 10;
-	int c = (x % 10);
+	int a = (x / 100); // finds the first number
+	int b = (x % 100) / 10; // finds the second number
+	int c = (x % 10); // finds the third number
 	if (a < b && b < c) {
 		cout << "Your digits are in ascending order from left to right." << endl;
-		}
+	}
 	else if (a > b && b > c) {
 		cout << "Your digits are in descending order from left to right." << endl;
 	}
-	else{
+	else {
 		cout << "Your digits are neither ascending or descending." << endl;
 	}
-	pause();
-
 }
+	pause(); //pauses to see the displayed text
+	
+	}
